6c788b88af023e6e30610c794a40b23f33c6bbb6
Rong <captrong@gooda.my>
2025-05-30 07:58:02 +0800

    feature/setup all components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Camp from "@/components/Camp";
+import Footer from "@/components/Footer";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Hero />
+      <Features />
+      <GetApp />
+      <Guide />
+      <Camp />
+      <Footer />
+    </>
+  );
 }
